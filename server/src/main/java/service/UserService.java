81470c817c616869899b67e49aa19df33dcaa054
Adam <adamross202@gmail.com>
2025-05-21 06:41:56 -0700

    Make records for requests and responses. Make UserService

---
+++ server/src/main/java/service/UserService.java
@@ -0,0 +1,64 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.UserDAO;
+import model.AuthData;
+import model.UserData;
+import service.requests.LoginRequest;
+import service.requests.LogoutRequest;
+import service.requests.RegisterRequest;
+import service.results.LoginResult;
+import service.results.LogoutResult;
+import service.results.RegisterResult;
+
+import java.util.UUID;
+
+public class UserService {
+    private final UserDAO userDAO;
+    private final AuthDAO authDAO;
+
+    public UserService(UserDAO userDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.authDAO = authDAO;
+    }
+
+    public RegisterResult register(RegisterRequest request) throws DataAccessException {
+        if (userDAO.getUser(request.username()) != null) {
+            throw new DataAccessException("Error: username already taken");
+        }
+
+        UserData user = new UserData(request.username(), request.password(), request.email());
+        userDAO.createUser(user);
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, request.username());
+        authDAO.createAuth(authData);
+
+        return new RegisterResult(request.username(), authToken);
+    }
+
+    public LoginResult login(LoginRequest request) throws DataAccessException {
+        UserData user = userDAO.getUser(request.username());
+        if (user == null || !user.password().equals(request.password())) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        String authToken = UUID.randomUUID().toString();
+        AuthData authData = new AuthData(authToken, request.username());
+        authDAO.createAuth(authData);
+
+        return new LoginResult(request.username(), authToken);
+    }
+
+    public LogoutResult logout(LogoutRequest request) throws DataAccessException {
+        AuthData authData = authDAO.getAuth(request.authToken());
+        if (authData == null) {
+            throw new DataAccessException("Error: unauthorized");
+        }
+
+        authDAO.deleteAuth(request.authToken());
+
+        return new LogoutResult();
+    }
+}
---
+++ server/src/main/java/service/requests/JoinGameRequest.java
@@ -0,0 +1,4 @@
+package service.requests;
+
+public record JoinGameRequest(String authToken, int gameID, String playerColor) {
+}
