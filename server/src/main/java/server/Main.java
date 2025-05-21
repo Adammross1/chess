ba33e08cb64f19b4c44a738e41a067688a5fe3e3
Adam <adamross202@gmail.com>
2025-05-21 07:09:12 -0700

    Move Main.java into server directory. Made UserHandler

--- server/src/main/java/server/Main.java
+++ server/src/main/java/server/Main.java
@@ -1,5 +1,6 @@
+package server;
+
 import chess.*;
-import server.Server;
 
 public class Main {
     public static void main(String[] args) {
---
+++ server/src/main/java/server/handlers/UserHandler.java
@@ -0,0 +1,78 @@
+package server.handlers;
+
+import com.google.gson.Gson;
+import dataaccess.DataAccessException;
+import service.UserService;
+import service.requests.LoginRequest;
+import service.requests.LogoutRequest;
+import service.requests.RegisterRequest;
+import service.results.LoginResult;
+import service.results.RegisterResult;
+import spark.Request;
+import spark.Response;
+
+import java.util.Map;
+
+public class UserHandler {
+    private final UserService userService;
+    private final Gson gson = new Gson();
+
+    public UserHandler(UserService userService) {
+        this.userService = userService;
+    }
+
+    public Object register(Request req, Response res) {
+        try {
+            RegisterRequest request = gson.fromJson(req.body(), RegisterRequest.class);
+            RegisterResult result = userService.register(request);
+            res.status(200);
+            return gson.toJson(result);
+        } catch (DataAccessException e) {
+            if (e.getMessage().contains("already taken")) {
+                res.status(403);
+            } else {
+                res.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        } catch (Exception e) {
+            res.status(400);
+            return gson.toJson(Map.of("message", "Error: bad request"));
+        }
+    }
+
+    public Object login(Request req, Response res) {
+        try {
+            LoginRequest request = gson.fromJson(req.body(), LoginRequest.class);
+            LoginResult result = userService.login(request);
+            res.status(200);
+            return gson.toJson(result);
+        } catch (DataAccessException e) {
+            if (e.getMessage().contains("unauthorized")) {
+                res.status(401);
+            } else {
+                res.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        } catch (Exception e) {
+            res.status(400);
+            return gson.toJson(Map.of("message", "Error: bad request"));
+        }
+    }
+
+    public Object logout(Request req, Response res) {
+        try {
+            String authToken = req.headers("authorization");
+            LogoutRequest request = new LogoutRequest(authToken);
+            userService.logout(request);
+            res.status(200);
+            return gson.toJson(Map.of());
+        } catch (DataAccessException e) {
+            if (e.getMessage().contains("unauthorized")) {
+                res.status(401);
+            } else {
+                res.status(500);
+            }
+            return gson.toJson(Map.of("message", e.getMessage()));
+        }
+    }
+}
