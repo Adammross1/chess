d2160599cc45c0f4aab8fd02d3ec5da6a5ebda9f
Adam <adamross202@gmail.com>
2025-05-21 06:55:38 -0700

    Make ClearService to clear all app data

---
+++ server/src/main/java/service/ClearService.java
@@ -0,0 +1,27 @@
+package service;
+
+import dataaccess.AuthDAO;
+import dataaccess.DataAccessException;
+import dataaccess.GameDAO;
+import dataaccess.UserDAO;
+import service.requests.ClearAppRequest;
+import service.results.ClearAppResult;
+
+public class ClearService {
+    private final UserDAO userDAO;
+    private final GameDAO gameDAO;
+    private final AuthDAO authDAO;
+
+    public ClearService(UserDAO userDAO, GameDAO gameDAO, AuthDAO authDAO) {
+        this.userDAO = userDAO;
+        this.gameDAO = gameDAO;
+        this.authDAO = authDAO;
+    }
+
+    public ClearAppResult clearApplication(ClearAppRequest request) throws DataAccessException {
+        userDAO.clear();
+        gameDAO.clear();
+        authDAO.clear();
+        return new ClearAppResult();
+    }
+}
