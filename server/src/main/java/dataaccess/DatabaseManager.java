bf7c64bf0e8541feb21b1159ab9f049723dc3d40
Adam <adamross202@gmail.com>
2025-05-27 22:28:52 -0700

    logic for initializing db and db tables

---
+++ server/src/main/java/dataaccess/DatabaseManager.java
@@ -0,0 +1,127 @@
+package dataaccess;
+
+import java.sql.*;
+import java.util.Properties;
+
+public class DatabaseManager {
+    private static String databaseName;
+    private static String dbUsername;
+    private static String dbPassword;
+    private static String connectionUrl;
+
+    /*
+     * Load the database information for the db.properties file.
+     */
+    static {
+        loadPropertiesFromResources();
+    }
+
+    /**
+     * Creates the database if it does not already exist.
+     */
+    static public void createDatabase() throws DataAccessException {
+        var statement = "CREATE DATABASE IF NOT EXISTS " + databaseName;
+        try (var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+             var preparedStatement = conn.prepareStatement(statement)) {
+            preparedStatement.executeUpdate();
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create database", ex);
+        }
+    }
+
+    /**
+     * Initializes the database and all required tables.
+     * This method is idempotent and can be called multiple times safely.
+     */
+    static public void initializeDatabase() throws DataAccessException {
+        // First create the database if it doesn't exist
+        createDatabase();
+
+        // Then create all tables
+        try (var conn = getConnection()) {
+            // Create user table
+            var createUserTable = """
+                CREATE TABLE IF NOT EXISTS user (
+                    username VARCHAR(255) PRIMARY KEY,
+                    password_hash VARCHAR(255) NOT NULL
+                )
+                """;
+            
+            // Create auth table
+            var createAuthTable = """
+                CREATE TABLE IF NOT EXISTS auth (
+                    auth_token VARCHAR(255) PRIMARY KEY,
+                    username VARCHAR(255) NOT NULL,
+                    FOREIGN KEY (username) REFERENCES user(username) ON DELETE CASCADE
+                )
+                """;
+            
+            // Create game table
+            var createGameTable = """
+                CREATE TABLE IF NOT EXISTS game (
+                    id INT AUTO_INCREMENT PRIMARY KEY,
+                    white_username VARCHAR(255),
+                    black_username VARCHAR(255),
+                    game_name VARCHAR(255) NOT NULL,
+                    game_state JSON NOT NULL,
+                    FOREIGN KEY (white_username) REFERENCES user(username) ON DELETE SET NULL,
+                    FOREIGN KEY (black_username) REFERENCES user(username) ON DELETE SET NULL
+                )
+                """;
+
+            try (var statement = conn.createStatement()) {
+                statement.execute(createUserTable);
+                statement.execute(createAuthTable);
+                statement.execute(createGameTable);
+            }
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to create tables", ex);
+        }
+    }
+
+    /**
+     * Create a connection to the database and sets the catalog based upon the
+     * properties specified in db.properties. Connections to the database should
+     * be short-lived, and you must close the connection when you are done with it.
+     * The easiest way to do that is with a try-with-resource block.
+     * <br/>
+     * <code>
+     * try (var conn = DatabaseManager.getConnection()) {
+     * // execute SQL statements.
+     * }
+     * </code>
+     */
+    static Connection getConnection() throws DataAccessException {
+        try {
+            //do not wrap the following line with a try-with-resources
+            var conn = DriverManager.getConnection(connectionUrl, dbUsername, dbPassword);
+            conn.setCatalog(databaseName);
+            return conn;
+        } catch (SQLException ex) {
+            throw new DataAccessException("failed to get connection", ex);
+        }
+    }
+
+    private static void loadPropertiesFromResources() {
+        try (var propStream = Thread.currentThread().getContextClassLoader().getResourceAsStream("db.properties")) {
+            if (propStream == null) {
+                throw new Exception("Unable to load db.properties");
+            }
+            Properties props = new Properties();
+            props.load(propStream);
+            loadProperties(props);
+        } catch (Exception ex) {
+            throw new RuntimeException("unable to process db.properties", ex);
+        }
+    }
+
+    private static void loadProperties(Properties props) {
+        databaseName = props.getProperty("db.name");
+        dbUsername = props.getProperty("db.user");
+        dbPassword = props.getProperty("db.password");
+
+        var host = props.getProperty("db.host");
+        var port = Integer.parseInt(props.getProperty("db.port"));
+        connectionUrl = String.format("jdbc:mysql://%s:%d", host, port);
+    }
+}
