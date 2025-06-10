48ddf4c518f2b100247c74505ee6ab34437223bb
Adam <adamross202@gmail.com>
2025-06-09 22:09:21 -0600

    Made UserGameCommand and ServerMessage extensions

--- shared/src/main/java/websocket/commands/UserGameCommand.java
+++ shared/src/main/java/websocket/commands/UserGameCommand.java
@@ -1,6 +1,7 @@
 package websocket.commands;
 
 import java.util.Objects;
+import chess.ChessMove;
 
 /**
  * Represents a command a user can send the server over a websocket
@@ -16,10 +17,25 @@ public class UserGameCommand {
 
     private final Integer gameID;
 
+    private final ChessMove move;
+
     public UserGameCommand(CommandType commandType, String authToken, Integer gameID) {
+        this(commandType, authToken, gameID, null);
+    }
+
+    public UserGameCommand(CommandType commandType, String authToken, Integer gameID, ChessMove move) {
         this.commandType = commandType;
         this.authToken = authToken;
         this.gameID = gameID;
+        this.move = move;
+    }
+
+    // No-args constructor for deserialization
+    public UserGameCommand() {
+        this.commandType = null;
+        this.authToken = null;
+        this.gameID = null;
+        this.move = null;
     }
 
     public enum CommandType {
@@ -41,6 +57,10 @@ public class UserGameCommand {
         return gameID;
     }
 
+    public ChessMove getMove() {
+        return move;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (this == o) {
@@ -52,11 +72,12 @@ public class UserGameCommand {
         UserGameCommand that = (UserGameCommand) o;
         return getCommandType() == that.getCommandType() &&
                 Objects.equals(getAuthToken(), that.getAuthToken()) &&
-                Objects.equals(getGameID(), that.getGameID());
+                Objects.equals(getGameID(), that.getGameID()) &&
+                Objects.equals(getMove(), that.getMove());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getCommandType(), getAuthToken(), getGameID());
+        return Objects.hash(getCommandType(), getAuthToken(), getGameID(), getMove());
     }
 }
--- shared/src/main/java/websocket/messages/ServerMessage.java
+++ shared/src/main/java/websocket/messages/ServerMessage.java
@@ -1,6 +1,7 @@
 package websocket.messages;
 
 import java.util.Objects;
+import chess.ChessGame;
 
 /**
  * Represents a Message the server can send through a WebSocket
@@ -10,6 +11,9 @@ import java.util.Objects;
  */
 public class ServerMessage {
     ServerMessageType serverMessageType;
+    private ChessGame game;
+    private String errorMessage;
+    private String message;
 
     public enum ServerMessageType {
         LOAD_GAME,
@@ -17,12 +21,47 @@ public class ServerMessage {
         NOTIFICATION
     }
 
-    public ServerMessage(ServerMessageType type) {
+    public ServerMessage(ServerMessageType type, ChessGame game) {
         this.serverMessageType = type;
+        this.game = game;
+    }
+
+    public ServerMessage(ServerMessageType type, String errorMessage, boolean isError) {
+        this.serverMessageType = type;
+        if (isError) {
+            this.errorMessage = errorMessage;
+        } else {
+            this.message = errorMessage;
+        }
+    }
+
+    public ServerMessage(ServerMessageType type, String message) {
+        this.serverMessageType = type;
+        this.message = message;
+    }
+
+    // No-args constructor for deserialization
+    public ServerMessage() {
+        this.serverMessageType = null;
+        this.game = null;
+        this.errorMessage = null;
+        this.message = null;
     }
 
     public ServerMessageType getServerMessageType() {
         return this.serverMessageType;
+    }
+
+    public ChessGame getGame() {
+        return game;
+    }
+
+    public String getErrorMessage() {
+        return errorMessage;
+    }
+
+    public String getMessage() {
+        return message;
     }
 
     @Override
@@ -34,11 +73,14 @@ public class ServerMessage {
             return false;
         }
         ServerMessage that = (ServerMessage) o;
-        return getServerMessageType() == that.getServerMessageType();
+        return getServerMessageType() == that.getServerMessageType() &&
+                Objects.equals(getGame(), that.getGame()) &&
+                Objects.equals(getErrorMessage(), that.getErrorMessage()) &&
+                Objects.equals(getMessage(), that.getMessage());
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(getServerMessageType());
+        return Objects.hash(getServerMessageType(), getGame(), getErrorMessage(), getMessage());
     }
 }
