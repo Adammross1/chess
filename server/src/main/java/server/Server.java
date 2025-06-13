297a6ddad0af63d91bf2ca315ee488403b35c05a
Adam <adamross202@gmail.com>
2025-06-12 18:04:36 -0600

    fix static files failing test

--- server/src/main/java/server/Server.java
+++ server/src/main/java/server/Server.java
@@ -61,7 +61,7 @@ public class Server {
     public int run(int desiredPort) {
         Spark.port(desiredPort);
 
-        // Spark.staticFiles.location("web");
+        Spark.staticFiles.location("web");
 
         registerEndpoints();
 
